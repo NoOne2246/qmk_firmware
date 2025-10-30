2c0c2c2d22a31456fe016ecebc0c0164a2e328f3
NoOne2246 <christopher.ho@hotmail.com>
2025-10-30 22:29:51 +1100

    Added Edmund's Custom Keyboard

---
+++ keyboards/monsgeek/m1_v5_custom/config.h
@@ -0,0 +1,23 @@
+// Copyright 2024 yangzheng20003 (@yangzheng20003)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define NKRO_DEFAULT_ON true
+
+/* Encoder */
+#define ENCODER_MAP_KEY_DELAY               1
+
+/* SPI */
+#define SPI_DRIVER                          SPIDQ
+#define SPI_SCK_PIN                         B3
+#define SPI_MOSI_PIN                        B5
+#define SPI_MISO_PIN                        B4
+
+/* Flash */
+#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
+#define WEAR_LEVELING_LOGICAL_SIZE          (WEAR_LEVELING_BACKING_SIZE / 2)
+
+/* WS2812 */
+#define WS2812_SPI_DRIVER  SPIDM2
+#define WS2812_SPI_DIVISOR 32
